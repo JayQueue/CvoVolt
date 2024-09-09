13165fbe9fbf59414b7ed0a4a20544113d9e0693
Jay Queue <jayqueue@Jays-Air.lan>
2024-09-09 19:36:43 +0200

    Workshop 1, opdracht 1

---
+++ Workshop_1/opdracht_1.py
@@ -0,0 +1,24 @@
+"""
+Auteur: Jonathan Quartier
+Datum: 9/9/2024
+Workshop 1: Opdracht 1
+"""
+def make_profile(vnaam: str, naam: str, minimum_extra_info=3, **extra_info):
+    if len(extra_info) >= minimum_extra_info:
+        profile = {
+            'voornaam': vnaam,
+            'naam': naam
+        }
+        profile.update(extra_info)
+        return profile
+    else:
+        return f"Geef minstens {minimum_extra_info} andere gegevens."
+
+def write_file(filename: str, data: str):
+    with open(filename, 'w') as file:
+        file.write(str(data))
+
+profile = make_profile('Jonathan', 'Quartier', leeftijd=46, woonplaats='Kortrijk')
+
+print(profile)
+write_file('profiel.txt', profile)
--- oef/oef1.py
+++
@@ -1,2 +0,0 @@
-print("Oefening 1")
-
